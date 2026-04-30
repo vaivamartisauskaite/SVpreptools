b42c0e9a908a3495933767530f47f0afb58cc8bb
vaivamartisauskaite <vaiva.martisauskaite@gmail.com>
2026-05-01 02:57:14 +0300

    Update config.js

--- locapp/config.js
+++ locapp/config.js
@@ -8,7 +8,7 @@ window.CFG = {
   SUPABASE_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdyc21zY3Jibnp0dGdwenJxenh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NzMzNTIsImV4cCI6MjA5MzA0OTM1Mn0.C1Qm2GpxJdkXqiWqehB0INWC8j4jyPyRDiE7W5lkiuA",  // Settings → API → anon public
 
   // Apps Script web app /exec URL (deploy from WebApp.gs)
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwg2UUaTtOEYKXZzoa6W6Xp2nB0iBJpg5xITfJnadDP52CeticVIIFojyxyp8O-tnxT/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyM0YdzCvTxJ70xZxIUEbIO1xsEE6EveaxvdIVQRG5JgpUO6mQTenwshs4nzodiSh5h/exec",
 
   // Same shared secret as in WebApp.gs (SHARED_SECRET)
   SHARED_SECRET: "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",
